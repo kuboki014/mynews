0a3b038f7b065efabe53d6dea67fe1bcfd6b6c48
窪木 <ayk.kuboki@gmail.com>
2023-08-13 14:15:19 +0900

    4-10課題4-10課題4-10課題4-10課題4-10課題4-10課題4-10課題4-10課題4-10課題4-10課題

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(NewsController::class)->prefix("admin")->name("news.")->group(function(){
+    Route::get("news/create", "add")->name("add");
+});
+
+Route::controller(SelfProfileController::class)->prefix("admin")->name("create.")->group(function(){
+    Route::get("profile/create", "add")->name("add");
+});
+
+Route::controller(SelfProfileController::class)->prefix("admin")->name("edit.")->group(function(){
+    Route::get("profile/edit", "edit")->name("edit");
+});
